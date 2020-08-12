2afacc8bcd13f5358f0cb1108b374713891d3852
Serhii Ilin <serhii.ilin@semanticbits.com>
2020-08-12 17:03:07 -0400

    MAT-1566 Extend TypeScript Models With Necessary Wrapper Classes - Partial implementation. Doesn't include population_set and etc

--- test/bundleParse.test.ts
+++ test/bundleParse.test.ts
@@ -1,5 +1,5 @@
 import "jest-extended";
-import {Bundle, IBundle, Patient, Resource} from "../src/models/fhir";
+import {Bundle, IBundle, Resource} from "../src/models/fhir";
 import bundleContents from "./resources/EXM104-9.1.000-bundle.json";
 
 describe("Parsing a FHIR bundle", () => {
